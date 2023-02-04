f591e90c84ebab4bfa5b77dc8501604d4a0fef67
BaoHoHo <giabaoho123@gmail.com>
2023-02-04 02:22:31 -0500

    finish the navigation bar

--- App.js
+++ App.js
@@ -1,31 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text, View ,Button} from 'react-native';
-import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator} from '@react-navigation/stack';
+import * as React from "react";
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./Components/HomeScreen";
+import AboutScreen from "./Components/AboutScreen";
 
-class HomeScreen extends React.Component {
-  constructor(props){
-    super(props);
-    this.state={}
-  }
-  render(){
-    return(
-      <View>
-      <View style={{margin:50}}>
-        <Button title="New Task"></Button>
-      </View>
-    </View>
-    )
-  }
-}
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const Stack = createStackNavigator();
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="About" component={AboutScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
