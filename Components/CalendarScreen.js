6dc0bc745ecc9c78f92980faef6ada131788cc9a
BaoHoHo <giabaoho123@gmail.com>
2023-02-04 14:43:48 -0500

    add rndate time picker

--- Components/CalendarScreen.js
+++ Components/CalendarScreen.js
@@ -1,11 +1,13 @@
 import React from "react";
-import { View} from "react-native";
+import {View} from "react-native";
 import { Calendar } from "react-native-calendars";
+import RNDateTimePicker from "@react-native-community/datetimepicker";
 
 export default function CalendarScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Calendar/>
-    </View>
-  );
+    return (
+        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+            <RNDateTimePicker value={new Date()} />
+            <Calendar/>
+        </View>
+    );
 }
